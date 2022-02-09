e2364ab8564bcb5d5c3eee507576f28a6a64ec97
phongnguyen <andycryptotrade@gmail.com>
2022-02-08 20:06:08 -0500

    split data into train, test set

--- model.py
+++ model.py
@@ -3,10 +3,49 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.models import Sequential
+from PIL import Image
+
 
 import glob, os, random
 
 base_path = r'/Users/phongnguyen/Documents/waste-classifier/dataset'
 
 img_list = glob.glob(os.path.join(base_path, '*/*.jpg'))
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True,
+    vertical_flip=True,
+    validation_split=0.1
+)
+
+test_datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.1
+)
+
+train_generator = train_datagen.flow_from_directory(
+    base_path,
+    target_size=(300, 300),
+    batch_size=16,
+    class_mode='categorical',
+    subset='training',
+    seed=0
+)
+
+validation_generator = test_datagen.flow_from_directory(
+    base_path,
+    target_size=(300, 300),
+    batch_size=16,
+    class_mode='categorical',
+    subset='validation',
+    seed=0
+)
+
+labels = (train_generator.class_indices)
+labels = dict((v,k) for k,v in labels.items())
 
