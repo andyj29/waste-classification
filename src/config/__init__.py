93f363341593377a741e0d520b0d3ddf484bfe7c
Phong Nguyen <93941060+andyj29@users.noreply.github.com>
2022-02-17 03:12:14 -0500

    switched model

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -3,4 +3,4 @@ from keras.models import load_model
 from .settings import BASE_DIR
 
 print('loading model ...')
-model = load_model(os.path.join(BASE_DIR,'config','new_model.h5'))
+model = load_model(os.path.join(BASE_DIR,'config','saved_model.h5'))
